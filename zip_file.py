1b955ca0f2fb40942f479924028cc87d90619b3f
nach1213 <nadavcherno@gmail.com>
2025-05-18 20:04:50 +0300

    mollli

--- zip_file.py
+++ zip_file.py
@@ -19,6 +19,26 @@ def rle(bytestr: bytes):
     retbytes += bytestr[-1:] + int.to_bytes(count, 1, "big")
     return retbytes
 
+def BWT(bytestr: bytes):
+    bytestr = b'^' + bytestr +b'$'
+    n = len(bytestr)
+    bwt = [''] * n
+    i = 0
+    for sa_i in SA(bytestr):
+        print(sa_i)
+        if sa_i == 0:
+            bwt[i] = bytestr[n - 1].to_bytes()
+        else:
+            bwt[i] = bytestr[sa_i - 1].to_bytes()
+        i += 1
+    return b''.join(bwt)
+
+def SA(bytestr: bytes):
+    n = len(bytestr)
+    suffixes = [(bytestr[-i:]+bytestr[:-i], i) for i in range(n)]
+    suffixes.sort(key=lambda pair: pair[0])
+    return [index for (_, index) in suffixes]
 
 if __name__ == "__main__":
     print(rle(b"A" * 260))
+    print(BWT(b"BANANA"))
