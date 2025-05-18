5995e68e37dcdf50d6f670e8f0043edae1531c9f
Itai Gani <itai@gani.org.il>
2025-05-18 19:18:43 +0300

    implement rle

--- zip_file.py
+++ zip_file.py
@@ -1,8 +1,24 @@
 def reverse_rle(bytestr: bytes):
-    retbytes = b''
+    retbytes = b""
     for i in range(1, len(bytestr), 2):
-        retbytes = retbytes + (bytestr[i])*int.to_bytes(bytestr[i-1], 1,"big")
+        retbytes = retbytes + (bytestr[i]) * int.to_bytes(bytestr[i - 1], 1, "big")
     return retbytes
 
+
+def rle(bytestr: bytes):
+    retbytes = b""
+    count = 1
+    for i in range(1, len(bytestr)):
+        if bytestr[i] == bytestr[i - 1] and count < 255:
+            count += 1
+        elif count > 1:
+            retbytes += bytestr[i - 1 : i] + int.to_bytes(count, 1, "big")
+            count = 1
+        else:
+            retbytes += bytestr[i - 1 : i]
+    retbytes += bytestr[-1:] + int.to_bytes(count, 1, "big")
+    return retbytes
+
+
 if __name__ == "__main__":
-    print(reverse_rle(b'A\x04B\x02C\x01D\x02'))
+    print(rle(b"A" * 260))
